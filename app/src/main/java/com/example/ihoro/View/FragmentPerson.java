f4cb634b0f52b2ccffc2a6615368647fc173f457
NickKen <hoanganh.nickken@gmail.com>
2023-04-03 04:13:06 +0700

    Update details: - Add:    + Change Color & Notice when client don't fill up all the blank - Fix:    + Constraint of some fragment layout.    + Separate data entry check.

--- app/src/main/java/com/example/ihoro/View/FragmentPerson.java
+++ app/src/main/java/com/example/ihoro/View/FragmentPerson.java
@@ -14,11 +14,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.ihoro.Controller.ShowResultPerson;
@@ -36,6 +38,10 @@ public class FragmentPerson extends Fragment {
     private DatePickerDialog.OnDateSetListener setDateListener;
     private TimePickerDialog.OnTimeSetListener setTimeListener;
     private String gender = "";
+    private TextView tv_warning_name;
+    private TextView tv_warning_date;
+    private TextView tv_warning_gender;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -47,23 +53,29 @@ public class FragmentPerson extends Fragment {
         et_Name = (EditText) view.findViewById(R.id.et_name);
         btn_Gender = (Button) view.findViewById(R.id.btn_gender);
         btn_showResult = (Button) view.findViewById(R.id.btn_result_person);
-
+        tv_warning_name = (TextView) view.findViewById(R.id.tv_warning_name);
+        tv_warning_date = (TextView) view.findViewById(R.id.tv_warning_date);
+        tv_warning_gender = (TextView) view.findViewById(R.id.tv_warning_gender);
         setDate();
 
         onClick();
         return view;
     }
-    public void onClick()
-    {
 
+    public void onClick() {
+
+        et_Name.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                et_Name.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.et_line));
+                tv_warning_name.setVisibility(View.INVISIBLE);
+            }
+        });
         btn_showResult.setOnClickListener(v -> {
             et_Name.clearFocus();
-            if(!checkFormInfo())
-            {
-                Toast.makeText(getActivity(), "Nhập thông tin sai format", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
+            if (!checkFormInfo()) {
+                Toast.makeText(getActivity(), "Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+            } else {
                 Intent intent = new Intent(getActivity(), ShowResultPerson.class);
                 intent.putExtra("Name", et_Name.getText().toString());
                 intent.putExtra("Gender", btn_Gender.getText().toString());
@@ -84,6 +96,9 @@ public class FragmentPerson extends Fragment {
         });
         btn_Date.setOnClickListener(v -> {
             et_Name.clearFocus();
+            btn_Date.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.et_line));
+            tv_warning_date.setVisibility(View.INVISIBLE);
+
             btn_Date.setShowSoftInputOnFocus(false);
             Calendar calendar = Calendar.getInstance();
             int year = calendar.get(Calendar.YEAR);
@@ -95,19 +110,22 @@ public class FragmentPerson extends Fragment {
         });
         btn_Gender.setOnClickListener(v -> {
             et_Name.clearFocus();
+            btn_Gender.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.et_line_small));
+            tv_warning_gender.setVisibility(View.INVISIBLE);
+
             showGenderDialog();
         });
     }
-    public void setDate()
-    {
+
+    public void setDate() {
         setDateListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int day) {
                 month = month + 1;
                 String formatDay = "" + day;
                 String formatMonth = "" + month;
-                if(day < 10) formatDay = "0" + day;
-                if(month < 10) formatMonth = "0" + month;
+                if (day < 10) formatDay = "0" + day;
+                if (month < 10) formatMonth = "0" + month;
                 Log.e("DATE: ", formatDay + "/" + formatMonth + "/" + year);
                 String date = formatDay + "/" + formatMonth + "/" + year;
                 btn_Date.setText(date);
@@ -118,8 +136,8 @@ public class FragmentPerson extends Fragment {
             public void onTimeSet(TimePicker view, int hour, int minute) {
                 String formatHour = "" + hour;
                 String formatMinute = "" + minute;
-                if(hour < 10) formatHour = "0" + hour;
-                if(minute < 10) formatMinute = "0" + minute;
+                if (hour < 10) formatHour = "0" + hour;
+                if (minute < 10) formatMinute = "0" + minute;
                 Log.e("DATE: ", formatHour + ":" + formatMinute);
                 String date = formatHour + ":" + formatMinute;
                 btn_Time.setText(date);
@@ -129,6 +147,7 @@ public class FragmentPerson extends Fragment {
         };
 
     }
+
     public void showGenderDialog() {
         final CharSequence[] items = {"Nam", "Nữ"};
 
@@ -145,8 +164,7 @@ public class FragmentPerson extends Fragment {
                 if (TextUtils.isEmpty(gender)) {
                     gender = "Nam";
                 }
-                // Thực hiện xử lý khi người dùng chọn giới tính
-                Toast.makeText(getActivity(), "Bạn đã chọn " + gender, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getActivity(), "Bạn đã chọn " + gender, Toast.LENGTH_SHORT).show();
                 btn_Gender.setText(gender.toLowerCase());
             }
         });
@@ -159,13 +177,29 @@ public class FragmentPerson extends Fragment {
         AlertDialog alert = builder.create();
         alert.show();
     }
-    private boolean checkFormInfo()
-    {
-        if(TextUtils.isEmpty(et_Name.getText())) return false;
-        if(TextUtils.isEmpty(btn_Date.getText())) return false;
-        if(TextUtils.isEmpty(btn_Gender.getText())) return false;
+
+    private boolean checkFormInfo() {
+        boolean checked = true;
+        if (TextUtils.isEmpty(et_Name.getText())) {
+            et_Name.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.et_line_warning));
+            tv_warning_name.setVisibility(View.VISIBLE);
+            checked = false;
+        }
+        if (TextUtils.isEmpty(btn_Date.getText())) {
+            btn_Date.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.et_line_warning));
+            tv_warning_date.setVisibility(View.VISIBLE);
+            checked = false;
+        }
+        if (TextUtils.isEmpty(btn_Gender.getText())) {
+            btn_Gender.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.et_line_small_warning));
+            tv_warning_gender.setVisibility(View.VISIBLE);
+            checked = false;
+        }
+
         String dateRegex = "[0-9]{2}.[0-9]{2}.[0-9]{1,4}";
-        if(!btn_Date.getText().toString().matches(dateRegex)) return false;
-        return true;
+        if (!btn_Date.getText().toString().matches(dateRegex)) {
+            checked = false;
+        }
+        return checked;
     }
 }
